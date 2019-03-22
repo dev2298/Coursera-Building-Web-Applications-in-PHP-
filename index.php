a16202daf28670c0354d818f57685537fd247936
DEV S <iamdevansh496@gmail.com>
2019-03-22 18:27:53 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>DEVANSH SRIVASTAVA PHP</title>
+</head>
+<body>
+  <h1>DEVANSH SRIVASTAVA php </h1>
+  <p>The SHA256 hash of "Devansh Srivastava" is <br> </p>
+  <?
+  print hash('sha256', 'Charles Severance');
+ ?>
+ <h2>ASCII ARt </h2>
+  <pre>
+    ________
+  \______ \
+   |    |  \
+   |    `   \
+  /_______  /
+          \/
+</pre><br>
+
+<a href="check.php">Click here to see the error settings</a><br>
+
+<a href="fail.php">Click here to cause a traceback</a>
+ </pre>
+</body>
+<html>
