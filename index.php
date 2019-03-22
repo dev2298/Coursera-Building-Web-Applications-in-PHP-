b1daa72a36cdd788be2d73f0e3c22d0218003c74
dev2298 <34762603+dev2298@users.noreply.github.com>
2019-03-22 18:41:21 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,79 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-  <title>DEVANSH SRIVASTAVA PHP</title>
-</head>
+<!DOCTYPE html>
+<head><title>DEVANSH SRIVASTAVA MD5 Cracker</title></head>
 <body>
-  <h1>DEVANSH SRIVASTAVA php </h1>
-  <p>The SHA256 hash of "Devansh Srivastava" is <br> </p>
-  <?
-  print hash('sha256', 'Charles Severance');
- ?>
- <h2>ASCII ARt </h2>
-  <pre>
-    ________
-  \______ \
-   |    |  \
-   |    `   \
-  /_______  /
-          \/
-</pre><br>
+<h1>MD5 cracker</h1>
+<p>This application takes an MD5 hash
+of a four-character lower case string and
+attempts to hash all two-character combinations
+to determine the original two characters.</p>
 
-<a href="check.php">Click here to see the error settings</a><br>
+Debug Output:
+<?php
+$goodtext = "Not found";
+// If there is no parameter, this code is all skipped
+if ( isset($_GET['md5']) ) {
+    $time_pre = microtime(true);
+    $md5 = $_GET['md5'];
 
-<a href="fail.php">Click here to cause a traceback</a>
- </pre>
+    // This is our alphabet
+    $txt = "1234567890";
+    $show = 15;
+
+    // Outer loop go go through the alphabet for the
+    // first position in our "possible" pre-hash
+    // text
+    for($i=0; $i<strlen($txt); $i++ ) {
+        $ch1 = $txt[$i];   // The first of two characters
+
+
+        for($j=0; $j<strlen($txt); $j++ ) {
+            $ch2 = $txt[$j];  // Our second character
+
+            for($j1=0; $j1<strlen($txt); $j1++ ) {
+                $ch3 = $txt[$j1];
+
+            for($j2=0; $j2<strlen($txt); $j2++ ) {
+                $ch4 = $txt[$j2];
+
+            $try = $ch1.$ch2.$ch3.$ch4;
+
+            // Run the hash and then check to see if we match
+            $check = hash('md5', $try);
+            if ( $check == $md5 ) {
+                $goodtext = $try;
+                break;   // Exit the inner loop
+            }
+
+            // Debug output until $show hits 0
+            if ( $show > 0 ) {
+                print "$check $try\n";
+                $show = $show - 1;
+            }
+        }
+    }
+  }
+}
+    // Compute elapsed time
+    $time_post = microtime(true);
+    print "Elapsed time: ";
+    print $time_post-$time_pre;
+    print "\n";
+}
+?>
+
+<!-- Use the very short syntax and call htmlentities() -->
+<p>Original Text: <?= htmlentities($goodtext); ?></p>
+<form>
+<input type="text" name="md5" size="60" />
+<input type="submit" value="Crack MD5"/>
+</form>
+<ul>
+<li><a href="index.php">Reset</a></li>
+<li><a href="md5.php">MD5 Encoder</a></li>
+<li><a href="makecode.php">MD5 Code Maker</a></li>
+<li><a
+href="https://github.com/csev/wa4e/tree/master/code/crack"
+target="_blank">Source code for this application</a></li>
+</ul>
 </body>
-<html>
+</html>
